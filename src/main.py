971b81c682c0d3ca766b84306d2c885b8242afdf
small-brain-ferret <yixuanxu20080107@gmail.com>
2023-07-13 19:08:05 +1000

    updated

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 
-loaded_model = keras.models.load_model('yicte-bushfires/model/model.keras')
+loaded_model = keras.models.load_model('model/model.keras')
 
 
 def fire_predict(max_temp_syd, max_temp_bne, year, month, day):
@@ -30,13 +30,6 @@ def fire_predict(max_temp_syd, max_temp_bne, year, month, day):
     return (f"""
             The scanned area of your fires was {predicted_fire_area} square kilometres. 
             This is {percentage}% of the Black Saturday week.""")
-
-# demo = gr.Interface(
-#     fn = your_function,
-#     inputs = ["number", "number", "number", "number", "number"],
-#     outputs = "number"
-# )
-
 
 with gr.Blocks() as demo:
     with gr.Row():
