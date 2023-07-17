4e1d0d1d06131a0324d511dfa65d65ef9f2b17b3
Joel <58995538+jtpotato@users.noreply.github.com>
2023-07-17 16:10:56 +1000

    update text formatting

--- src/firetrace/predict.py
+++ src/firetrace/predict.py
@@ -3,16 +3,18 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 
-loaded_model = keras.models.load_model('model/model.keras')
+loaded_model = keras.models.load_model("model/model.keras")
+
 
 def ui_predict(max_temp_syd, max_temp_bne, year, month, day):
     predictions = fire_predict(max_temp_syd, max_temp_bne, year, month, day)
     additional = additional_context(predictions)
     return predictions, additional
 
+
 def fire_predict(max_temp_syd, max_temp_bne, year, month, day):
     date = datetime(int(year), int(month), int(day))
-    day_of_year = int(date.strftime('%j'))
+    day_of_year = int(date.strftime("%j"))
 
     time_sin = np.sin((day_of_year / 365) * 2 * np.pi)
     time_cos = np.cos((day_of_year / 365) * 2 * np.pi)
@@ -22,7 +24,7 @@ def fire_predict(max_temp_syd, max_temp_bne, year, month, day):
         "max_t_bne": [max_temp_bne],
         "year": [year - 2000],
         "time_sin": [time_sin],
-        "time_cos": [time_cos]
+        "time_cos": [time_cos],
     }
     data_df = pd.DataFrame(data=data)
 
@@ -35,6 +37,4 @@ def additional_context(scan_area):
     LARGEST_EVENT = 2505.371429
 
     percentage = round((scan_area / LARGEST_EVENT) * 100, 2)
-    return (f"""
-            The scanned area of your fires was {scan_area} square kilometres. 
-            This is {percentage}% of the largest fire event in our database, at {LARGEST_EVENT} square kilometres, recorded on the week of the 25th of September, 2011.""")
+    return f"""The scanned area of your fires was {scan_area} square kilometres. This is {percentage}% of the largest fire event in our database, at {LARGEST_EVENT} square kilometres, recorded on the week of the 25th of September, 2011."""
