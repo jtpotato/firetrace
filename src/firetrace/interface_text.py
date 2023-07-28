9ee02105005cda1d97ae0f14956359f691bce292
Joel <58995538+jtpotato@users.noreply.github.com>
2023-07-28 21:27:02 +1000

    add some comparisons

--- src/firetrace/interface_text.py
+++ src/firetrace/interface_text.py
@@ -23,3 +23,28 @@ q_and_a = """
 
         Let's work together to face these fiery challenges to create a safer future! 🚀🌿
         """
+
+def additional_context(scan_area):
+    def get_percentage(scan_area, area):
+        return round((scan_area / area) * 100, 2)
+    
+    def get_times(scan_area, area):
+        return round(scan_area / area, 2)
+    
+    LARGEST_EVENT = 5854.7
+    MELBOURNE_AREA = 9993
+    PORT_JACKSON_BAY_AREA = 55
+    MURRAY_DARLING_BASIN_AREA = 1059000
+    ACT_AREA = 2400
+
+    context_string = f"""
+        The predicted area of your fires 🤓 was `{round(scan_area, 2)}` square kilometres. 🤯 This is `{get_percentage(scan_area, LARGEST_EVENT)}%` of the largest fire event 🔥 in our database, at {round(LARGEST_EVENT, 2)} square kilometres, recorded on the 19th of September 2011.
+        
+        ### Other things this fire compares to:
+        - `{get_percentage(scan_area, MELBOURNE_AREA)}%` of Greater Melbourne. 😧
+        - `{get_times(scan_area, PORT_JACKSON_BAY_AREA)}` Port Jackson Bays 🌊
+        - `{get_percentage(scan_area, MURRAY_DARLING_BASIN_AREA)}%` of the Murray-Darling Basin. 🌾
+        - `{get_percentage(scan_area, ACT_AREA)}%` of the ACT. 🏙️
+        """
+
+    return context_string
