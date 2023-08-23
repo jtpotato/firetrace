fdc16ee409e816da559fe199f095a9179cd6d7e0
Joel <58995538+jtpotato@users.noreply.github.com>
2023-08-23 23:23:32 +1000

    add analytics

--- firetrace/js/onload.js
+++ firetrace/js/onload.js
@@ -2,4 +2,10 @@ window.onload = function () {
   console.log("Hi! We're just here to force dark mode because we like it. We don't believe that there will be any accessibility issues with this, but if there are, please let us know by filing an Issue on GitHub or HuggingFace. https://github.com/jtpotato/firetrace/issues/new")
   document.body.classList.add("dark")
   document.body.style.backgroundColor = "#0b0f19"
+
+  // get user agent
+  let ua = navigator.userAgent.toLowerCase();
+  // get time zone name (for geographic location reasons)
+  let timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+  fetch("https://firetrace-analytics.joelzktan5697.workers.dev/?user_agent=" + ua + "&timezone_name=" + timezone) // log to analytics endpoint
 }
