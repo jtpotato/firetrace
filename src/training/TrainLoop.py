5a946043b9d5e142f4d212fc1546e0e7d7114c0d
Joel <58995538+jtpotato@users.noreply.github.com>
2023-12-15 17:45:00 +1100

    conclude that mps is too slow for this model

--- src/training/TrainLoop.py
+++ src/training/TrainLoop.py
@@ -3,16 +3,16 @@ import torch.nn as nn
 
 device=torch.device("cpu")
 
-if torch.backends.mps.is_available() and torch.backends.mps.is_built():
-    print("Using MPS.")
-    device = torch.device("mps")
+# if torch.backends.mps.is_available() and torch.backends.mps.is_built():
+#     print("Using MPS.")
+#     device = torch.device("mps")
 
-def train_loop(X_train, X_test, y_train, y_test, train_loader, compiled_model, saved_epochs, additional_epochs):
-    compiled_model.to(device)
+def train_loop(X_train, X_test, y_train, y_test, train_loader, firetrace_model, saved_epochs, additional_epochs):
+    firetrace_model.to(device)
 
-    loss_function = torch.compile(nn.MSELoss(), backend="aot_eager")
+    loss_function = nn.MSELoss()
 
-    optimizer = torch.optim.Adam(compiled_model.parameters(), lr=0.00001)
+    optimizer = torch.optim.Adam(firetrace_model.parameters(), lr=0.00001)
 
     for epoch in range(saved_epochs, saved_epochs + additional_epochs):
         epoch_loss = 0.0
@@ -26,7 +26,7 @@ def train_loop(X_train, X_test, y_train, y_test, train_loader, compiled_model, s
             # Zero the gradients
             optimizer.zero_grad()
 
-            outputs = compiled_model(inputs)
+            outputs = firetrace_model(inputs)
 
             loss = loss_function(outputs, targets)
 
@@ -38,7 +38,7 @@ def train_loop(X_train, X_test, y_train, y_test, train_loader, compiled_model, s
 
         if epoch % 100 == 0:
             # Validate model
-            test_output = compiled_model(torch.tensor(X_test).float().to(device))
+            test_output = firetrace_model(torch.tensor(X_test).float().to(device))
             test_loss = loss_function(test_output, torch.tensor(y_test, dtype=torch.float32, device=device).float()).item()
 
             print(
