2ddd80646a9a2ea132bb2fddd2ec6e058e9203b0
Joel <58995538+jtpotato@users.noreply.github.com>
2023-12-15 13:42:41 +1100

    fire heatmap

--- src/frontend/get_ui_prediction.py
+++ src/frontend/get_ui_prediction.py
@@ -2,6 +2,7 @@
 
 from inference.day_to_signal import day_to_signal
 from inference.get_prediction import get_prediction
+from frontend.additional_context import additional_context
 
 
 def get_ui_prediction(soi, max_t_bne, max_t_mel, max_t_cns, max_t_pth, max_t_syd, day, month, year):
@@ -9,4 +10,8 @@ def get_ui_prediction(soi, max_t_bne, max_t_mel, max_t_cns, max_t_pth, max_t_syd
     Process predictions from the UI, using the variables from the UI.
     """
     sin_signal, cos_signal = day_to_signal(day, month)
-    return f"{get_prediction(soi, max_t_bne, max_t_mel, max_t_cns, max_t_pth, max_t_syd, sin_signal, cos_signal, year)}"
+
+    fire_size = get_prediction(soi, max_t_bne, max_t_mel, max_t_cns, max_t_pth, max_t_syd, sin_signal, cos_signal, year)
+
+    context_string = additional_context(fire_size)
+    return f"{context_string}"
