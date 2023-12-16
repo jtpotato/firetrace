313dbebba8534dc2a6f1c5436c5657af559191d9
Joel <58995538+jtpotato@users.noreply.github.com>
2023-12-16 14:30:46 +1100

    update gradio interface

---
+++ src/frontend/js/onload.js
@@ -0,0 +1,7 @@
+function test() {
+  console.log(
+    "Hi! We're just here to force dark mode because we like it. We don't believe that there will be any accessibility issues with this, but if there are, please let us know by filing an Issue on GitHub or HuggingFace. https://github.com/jtpotato/firetrace/issues/new"
+  );
+  document.body.classList.add("dark");
+  document.body.style.backgroundColor = "#0b0f19";
+}
