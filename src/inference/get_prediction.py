bd909a47353510cb48f3daec0d278d8e4f67a6f6
Joel <58995538+jtpotato@users.noreply.github.com>
2023-12-16 15:49:39 +1100

    last-minute remove compilation for huggingface

--- src/inference/get_prediction.py
+++ src/inference/get_prediction.py
@@ -6,10 +6,10 @@ from training.FiretraceMLP import FiretraceMLP
 checkpoint = torch.load("models/firetrace_model.pt")
 
 model = FiretraceMLP(width=checkpoint['model_size'][0], depth=checkpoint['model_size'][1])
-compiled_model = torch.compile(model, fullgraph=True, mode="max-autotune")
+# compiled_model = torch.compile(model, fullgraph=True, mode="max-autotune")
 
-compiled_model.load_state_dict(checkpoint['model_state_dict'])
-compiled_model.eval()
+model.load_state_dict(checkpoint['model_state_dict'])
+model.eval()
 
 x_scaler = joblib.load("models/x_scaler.save")
 y_scaler = joblib.load("models/y_scaler.save")
@@ -18,7 +18,7 @@ def get_prediction(soi, max_t_bne, max_t_mel, max_t_cns, max_t_pth, max_t_syd, s
     inputs = [soi, max_t_bne, max_t_mel, max_t_cns, max_t_pth, max_t_syd, sin_month, cos_month, year]
     scaled_inputs = x_scaler.transform(np.array(inputs).reshape(1, -1))
 
-    model_output = compiled_model(torch.tensor(scaled_inputs).float())
+    model_output = model(torch.tensor(scaled_inputs).float())
 
     scaled_output = y_scaler.inverse_transform(model_output.detach().numpy().reshape(1, -1))[0][0]
     scaled_output = max(scaled_output, 0) # prevent negatives.
